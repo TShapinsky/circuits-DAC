7a41366587df62f8d5314eb0cf2ca283c67bd10e
Adam Selker <adamselker@gmail.com>
2019-05-07 16:30:58 -0400

    plot tweaks

--- plot.py
+++ plot.py
@@ -8,22 +8,23 @@ import matplotlib.pyplot as plt
 xs, ys = [], []
 with open(sys.argv[1]) as f:
     c = csv.reader(f, delimiter="\t")
-    
-    headers = next(c) # Throw away the header
+
+    headers = next(c)  # Throw away the header
     series = {}
     for header in headers:
         series[header] = np.array([])
     for row in c:
         for i in range(len(row)):
-            
-            series[headers[i]] = np.append(series[headers[i]],float(row[i]))
 
-for data in sys.argv[2:]:
-    if data == '*':
-        for header in headers[1:]:
-            plt.plot(series['time'],series[header],'.', label=header)
-        continue
-    plt.plot(series['time'], series[data], '.', label=data)
+            series[headers[i]] = np.append(series[headers[i]], float(row[i]))
+
+series_to_plot = headers[1:] if sys.argv[2] == "*" else sys.argv[2:]
+for data in series_to_plot:
+    plt.plot(series["time"], series[data], ".", label=data)
+
+plt.title("Simulated DAC characteristics")
+plt.xlabel("Input Number")
+plt.ylabel("Output voltage (V)")
+plt.grid(True)
 plt.legend()
 plt.show()
-
